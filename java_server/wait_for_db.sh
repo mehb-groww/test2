68184b1526f8e29da6e470e89abbcaa483ecd54e
Nguyen Duc Anh <adnguyen@Nguyens-MacBook-Pro-2.local>
2019-06-19 20:06:25 +0700

    fixing wait_for_db

--- java_server/wait_for_db.sh
+++ java_server/wait_for_db.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "Waiting for mysql"
-until mysql -h hackteeth_db -P 3306 -u root  &> /dev/null
+until (echo > /dev/tcp/$DB_ADDRESS/$DB_PORT) >/dev/null 2>&1
 do
   printf "."
   sleep 1
 done
 
 echo -e "\nmysql ready"
+sed -i 's#to_be_replaced#'"$DB_ADDRESS\:$DB_PORT"'#g' /root/hack_my_teeth/src/main/resources/application.properties
+java -jar -D/spring.profiles.active=docker /root/hack_my_teeth/target/HackMyTeeth-0.0.1-SNAPSHOT.jar
