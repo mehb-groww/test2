c3e8876a3eccce1ffff24cb923496d824b5f1285
ledz1996 <ledz1996@gmail.com>
2019-06-20 17:21:03 +0700

    update fixing

--- java_server/modified_db.sh
+++ java_server/modified_db.sh
@@ -1,1 +1,1 @@
-sed -i 's#to_be_replaced#'"$DB_ADDRESS\:$DB_PORT"'#g' /root/hack_my_teeth/src/main/resources/application.properties
+sed -i 's#to_be_replaced#'"$DB_ADDRESS:$DB_PORT"'#g' /root/hack_my_teeth/src/main/resources/application.properties
