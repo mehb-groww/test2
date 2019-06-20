3d00450af0bc9007a88a6ca7045db692572e9783
ledz1996 <ledz1996@gmail.com>
2019-06-20 14:20:50 +0700

    fixing stuffs

--- java_server/wait_for_db.sh
+++ java_server/wait_for_db.sh
@@ -8,5 +8,5 @@ do
 done
 
 echo -e "\nmysql ready"
-sed -i 's#to_be_replaced#'"$DB_ADDRESS\:$DB_PORT"'#g' /root/hack_my_teeth/src/main/resources/application.properties
+
 java -jar -D/spring.profiles.active=docker /root/hack_my_teeth/target/HackMyTeeth-0.0.1-SNAPSHOT.jar
